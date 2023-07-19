65373cc7f07b75880b940fc7b2e4433ae61a9fee
kobachajchir <kobachajchir2013@gmail.com>
2023-07-19 01:36:37 -0300

    favorites working

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,8 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useParams, Link } from "react-router-dom";
 import { ItemCount } from "./ItemCount";
 import { Col } from "react-bootstrap";
-import { Star, StarFill } from "react-bootstrap-icons";
+import { Heart, HeartFill, Star, StarFill } from "react-bootstrap-icons";
+import FavoriteHeart from "./FavoriteHeart";
 
 function Item(props) {
   const imgUrl = new URL(props.item.picture[0], import.meta.url).href;
@@ -15,14 +16,22 @@ function Item(props) {
       className="card themeEmphasisBgColor"
       key={props.index}
       style={{ margin: "5px" }}
-      data-bs-theme="dark"
     >
       <LazyLoadImage
         className="card-img-top"
         alt={props.item.code + "ProdImg"}
         src={imgUrl}
+        style={{
+          transform: "translateY(10px)",
+          borderRadius: "var(--bs-card-inner-border-radius)",
+        }}
       />
-      <div className="card-body">
+      <div
+        className="card-body"
+        style={{
+          transform: "translateY(5px)",
+        }}
+      >
         <div
           className="d-flex flex-row align-items-center"
           style={{ justifyContent: "space-between" }}
@@ -32,7 +41,13 @@ function Item(props) {
           </h5>
           <div className="d-flex flex-row align-items-center">
             <StarFill color="#CD9D00"></StarFill>
-            <p style={{ margin: 0 }}>{props.item.rating}</p>
+            <p style={{ margin: 0, marginLeft: "1px", marginRight: "10px" }}>
+              {props.item.rating}
+            </p>
+            <FavoriteHeart
+              isFavorite={props.isFavorite}
+              productCode={props.item.code}
+            ></FavoriteHeart>
           </div>
         </div>
         <p className="card-text">{props.item.description}</p>
